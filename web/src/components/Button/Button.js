8c69d9669c9e0303e88498e205606e32de5cfb1a
flippedcoder <milecia.mcgregor@aol.com>
2021-07-25 09:06:51 -0500

    first commit

---
+++ web/src/components/Button/Button.js
@@ -0,0 +1,5 @@
+// Button.stories.js | Button.stories.jsx
+
+export const Primary = () => <button background="#ff0">Primary Button</button>;
+export const Secondary = () => <button background="#ff0">😄👍😍💯</button>;
+export const Tertiary = () => <button background="#ff0">📚📕📈🤓</button>;
---
+++ web/src/components/Button/Button.stories.js
@@ -0,0 +1,13 @@
+import { Primary, Secondary, Tertiary } from './Button'
+
+export const generated = () => {
+  return (
+    <>
+      <Primary />
+      <Secondary />
+      <Tertiary />
+    </>
+  )
+}
+
+export default { title: 'Components/Button' }
